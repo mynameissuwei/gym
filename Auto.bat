ab0908c2bc73596ecb7780f5d7a2535bfcf7ee3f
suwei <910933955@qq.com>
2019-10-11 21:06:56 +0800

    A commit a day keeps the girlfriend away.

--- Auto.bat
+++ Auto.bat
@@ -1,6 +1,9 @@
 @echo off
 @title Auto Green Point
 
+netsh wlan disconnect
+netsh wlan connect ssid=1502 name=1502
+
 echo *>>README.md
 git add .
 git commit -m "A commit a day keeps the girlfriend away."
